d1f87ee83c67103b7a734e9d60e9f8618b5d272e
itsNiazi <niazitareqmejl@gmail.com>
2025-05-18 14:21:04 +0200

    Updated myConfig

---
+++ src/config/myConfig.ts
@@ -0,0 +1,19 @@
+export const myConfig = {
+  name: "Tareq Niazi",
+  role: "Software Developer - Fullstack",
+  subRole: "Information Architect",
+  location: "Malmö, Sweden",
+  age: new Date().getFullYear() - 1996,
+  social: { linkedin: "", github: "" },
+  contact: { email: "niazitareqmejl@gmail.com", phone: +46762760140 },
+  resume: {
+    path: "",
+  },
+  education: {
+    degree: "Bachelor of Science in Computer and Information Science",
+    institution: "Malmö University",
+    year: "2022-2025",
+    description: "",
+  },
+  experience: [{}],
+};
