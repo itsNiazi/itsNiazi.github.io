094d6113e0de1f006a5831545485805d43f09602
itsNiazi <niazitareqmejl@gmail.com>
2025-06-16 10:46:22 +0200

    Article details component initial work

--- src/config/myConfig.ts
+++ src/config/myConfig.ts
@@ -4,7 +4,7 @@ export const myConfig = {
   stack: "Fullstack",
   role: "Information Architect",
   location: "Malmö, Sweden",
-  age: new Date().getFullYear() - 1996, //not dynamic, because build must be in client-side script tag?
+  age: new Date().getFullYear() - 1996, //not dynamic in build?
   social: { linkedin: "https://linkedin.com/in/tareq-niazi-929b7a1a5", github: "https://github.com/itsNiazi" },
   contact: { email: "niazitareqmejl@gmail.com" },
   resume: {
